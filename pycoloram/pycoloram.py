121158b63842b5ab100b33c93ed80d31113cc0a1
amfili <75899059+amfili@users.noreply.github.com>
2020-12-13 19:18:12 +0300

    Add files via upload

--- pycoloram/pycoloram.py
+++ pycoloram/pycoloram.py
@@ -1,24 +1,28 @@
 class color:
-  def Black(text):
-    print("\033[30m{}\033[0m" .format(text))
-    
-  def Red(text):
-    print("\033[31m{}\033[0m" .format(text))
   
-  def Green(text):
-    print("\033[32m{}\033[0m" .format(text))
-    
-  def Yellow(text):
-    print("\033[33m{}\033[0m" .format(text))
-    
-  def Blue(text):
-    print("\033[34m{}\033[0m" .format(text))
-    
-  def Мagenta(ext):
-    print("\033[35m{}\033[0m" .format(text))
+  def __init__(self, text):
+    self.text = text
   
-  def Cyan(text):
-    print("\033[36m{}" .format(text))
+  def Black(self):
+    print("\033[30m{}\033[0m" .format(self.text))
+    
+  def Red(self):
+    print("\033[31m{}\033[0m" .format(self.text))
   
-  def White(text):
-    print("\033[37m{}" .format(text))
+  def Green(self):
+    print("\033[32m{}\033[0m" .format(self.text))
+    
+  def Yellow(self):
+    print("\033[33m{}\033[0m" .format(self.text))
+    
+  def Blue(self):
+    print("\033[34m{}\033[0m" .format(self.text))
+    
+  def Мagenta(self):
+    print("\033[35m{}\033[0m" .format(self.text))
+  
+  def Cyan(self):
+    print("\033[36m{}" .format(self.text))
+  
+  def White(self):
+    print("\033[37m{}" .format(self.text))
