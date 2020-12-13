498d744757ac8c79f7a36c48f53be52f6ab3f01b
amfili <75899059+amfili@users.noreply.github.com>
2020-12-13 18:22:14 +0300

    Add files via upload

---
+++ pycoloram/pycoloram.py
@@ -0,0 +1,24 @@
+class color:
+  def Black(text):
+    print("\033[30m{}\033[0m" .format(text))
+    
+  def Red(text):
+    print("\033[31m{}\033[0m" .format(text))
+  
+  def Green(text):
+    print("\033[32m{}\033[0m" .format(text))
+    
+  def Yellow(text):
+    print("\033[33m{}\033[0m" .format(text))
+    
+  def Blue(text):
+    print("\033[34m{}\033[0m" .format(text))
+    
+  def Мagenta(ext):
+    print("\033[35m{}\033[0m" .format(text))
+  
+  def Cyan(text):
+    print("\033[36m{}" .format(text))
+  
+  def White(text):
+    print("\033[37m{}" .format(text))
